4c1ca3ab22f816c8a4c2af4b5c599975bee3c920
vignesh <vigneshbalusami@gmail.com>
2017-11-15 09:24:05 +0530

    Rare-Files-Connector

--- learn/src/com/emp/main/Solution.java
+++ learn/src/com/emp/main/Solution.java
@@ -16,22 +16,31 @@ public class Solution {
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
 		session.beginTransaction();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter Id : ");
+		int id = Integer.valueOf(bf.readLine());
+		System.out.print("Enter the name : ");
+		String name = bf.readLine();
+		System.out.print("Enter the email : ");
+		String emailId = bf.readLine();
+		System.out.print("Enter phone Number : ");
+		long phone = Long.valueOf(bf.readLine());
+		System.out.print("Enter the Basic Salary : ");
+		float basicSalary = Float.parseFloat(bf.readLine());
+		System.out.print("Enter the DearnessAllowance : ");
+		float dearnessAllowance = Float.parseFloat(bf.readLine());
+		System.out.print("Enter the HouseRentAllownace : ");
+		float houseRentAllowance = Float.parseFloat(bf.readLine());
+		System.out.print("Enter the travelAllownace : ");
+		float travelAllowance = Float.parseFloat(bf.readLine());
+		System.out.print("Enter the ProvidentFund : ");
+		float providentFund = Float.parseFloat(bf.readLine());
+		System.out.print("Enter the Gender : ");
+		//boolean gender = 
 		Employee employee = new Employee();
-		/*
-		 * BufferedReader bf = new BufferedReader(new
-		 * InputStreamReader(System.in)); System.out.print("Enter Id : "); int
-		 * id = Integer.valueOf(bf.readLine());
-		 * System.out.print("Enter the name : "); String name = bf.readLine();
-		 * System.out.print("Enter the email : "); String emailId =
-		 * bf.readLine(); System.out.print("Enter phone Number : "); long phone
-		 * = Long.valueOf(bf.readLine()); Employee employee = new Employee(id,
-		 * name, emailId, phone); System.out.println(employee.getId() + " " +
-		 * employee.getName() + " " + employee.getEmailId() + " " +
-		 * employee.getPhone() );*/
 		session.save(employee);
-		
+
 		session.getTransaction().commit();
 		session.close();
 	}
-
 }
--- learn/src/com/emp/sample/Employee.java
+++ learn/src/com/emp/sample/Employee.java
@@ -2,6 +2,7 @@ package com.emp.sample;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -9,7 +10,9 @@ import javax.persistence.Transient;
 @Entity
 @Table(name = "Employee_Detail")
 public class Employee {
+
 	@Id
+	@GeneratedValue
 	@Column(name = "Employee_Id")
 	private int id;
 	@Column(name = "name", length = 250, nullable = false)
@@ -31,16 +34,70 @@ public class Employee {
 	@Transient
 	private boolean gender;
 
-	public Employee() {
-	}
-
-	public Employee(int id, String name, String emailId, long phone) {
+	public Employee(int id, String name, String emailId, long phone, float basicSalary, float dearnessAllovance,
+			float houseRentAllowance, float travelAllovance, float providFund, boolean gender) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.emailId = emailId;
 		this.phone = phone;
+		this.basicSalary = basicSalary;
+		this.dearnessAllovance = dearnessAllovance;
+		this.houseRentAllowance = houseRentAllowance;
+		this.travelAllovance = travelAllovance;
+		this.providFund = providFund;
+		this.gender = gender;
+	}
 
+	public float getBasicSalary() {
+		return basicSalary;
+	}
+
+	public void setBasicSalary(float basicSalary) {
+		this.basicSalary = basicSalary;
+	}
+
+	public float getDearnessAllovance() {
+		return dearnessAllovance;
+	}
+
+	public void setDearnessAllovance(float dearnessAllovance) {
+		this.dearnessAllovance = dearnessAllovance;
+	}
+
+	public float getHouseRentAllowance() {
+		return houseRentAllowance;
+	}
+
+	public void setHouseRentAllowance(float houseRentAllowance) {
+		this.houseRentAllowance = houseRentAllowance;
+	}
+
+	public float getTravelAllovance() {
+		return travelAllovance;
+	}
+
+	public void setTravelAllovance(float travelAllovance) {
+		this.travelAllovance = travelAllovance;
+	}
+
+	public float getProvidFund() {
+		return providFund;
+	}
+
+	public void setProvidFund(float providFund) {
+		this.providFund = providFund;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public Employee() {
 	}
 
 	public int getId() {
